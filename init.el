c151315f7dec21087a7c33813ac492295e39adb2
Sayre Blades <sayre.blades@gmail.com>
2025-08-09 11:00:36 -0400

    add zig support

--- init.el
+++ init.el
@@ -98,7 +98,7 @@
        (eval +overlay)     ; run code, run (also, repls)
        lookup              ; navigate your code and its documentation
        ;;llm               ; when I said you needed friends, I didn't mean...
-       ;;lsp               ; M-x vscode
+       lsp               ; M-x vscode
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
@@ -176,7 +176,8 @@
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;web               ; the tubes
        ;;yaml              ; JSON, but readable
-       ;;zig               ; C, but simpler
+       ;; zig               ; C, but simpler
+       (zig +lsp)          ; C, but simpler
 
        :email
        ;;(mu4e +org +gmail)
