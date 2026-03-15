994ac223934bc5b3d31bfe332798dc292cf32e27
Sayre Blades <sayre.blades@gmail.com>
2026-03-15 14:59:07 -0400

    style: switch theme to doom-nord-light
    2026-03-15
    Change active theme from doom-nord to doom-nord-light.
    Keep doom-nord as a commented alternative.

--- config.el
+++ config.el
@@ -34,7 +34,8 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 ;;(setq doom-theme 'doom-one)
-(setq doom-theme 'doom-nord)
+(setq doom-theme 'doom-nord-light)
+;; (setq doom-theme 'doom-nord)
 ;; (setq doom-theme 'doom-earl-grey)
 ;; (setq doom-theme 'doom-zenburn)
 
